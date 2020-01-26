d0372dd51873c69e77ea8a121780cc4534f02927
filippovk997 <filippovk997@mail.ru>
2020-01-26 19:08:57 +0400

    add router-dom; add completed task page; add deleting a favorite parameter when clicking on the checkbox.

--- src/App.js
+++ src/App.js
@@ -1,98 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import Header from './Header';
-import List from './List';
-import axios from 'axios';
+import React from 'react';
+import { 
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink
+} from 'react-router-dom';
+import Main from './pages/Main';
+import CompletedTasks from './pages/CompletedTasks';
 import './App.scss';
 
 export default function App() {
-    const [task, setTask] = useState("");
-    const [items, setItems] = useState([]);
-    
-    axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-    
-    const handleChangeTask = (event) => {
-        setTask(event.target.value);
-    };
-    useEffect(() => {
-        axios.get(`${axios.defaults.baseURL}`)
-        .then(response => {
-            setItems(response.data);
-        });
-    }, [])
-
-    const handleClickAdd = (value) => {
-        if (value === "")
-            return;
-
-        axios.post(`${axios.defaults.baseURL}`, { 
-                value: value, 
-                checked: false,
-                favorite: false 
-            })
-            .then(response => {
-                setItems(items.concat(response.data));
-            });
-
-        setTask("");
-    };
-
-    const handleKeyPressEnter = (event) => {
-        if (event.key === "Enter")
-            handleClickAdd(event.target.value);
-    };
-
-    const handleChangeChecked = (event) => {
-        setItems(items.map((item, index) => {
-            if (index == event.target.name) {
-                axios.patch(`${axios.defaults.baseURL}/${(item.id)}`, { checked: event.target.checked });
-            }
-            item.checked = (index == event.target.name) ? event.target.checked : item.checked;
-            return item;
-        }));
-    };
-
-    const handleChangeItems = (event) => {
-        setItems(items.map((item, index) => {
-            if (index == event.target.name) {
-                axios.patch(`${axios.defaults.baseURL}/${(item.id)}`, { value: event.target.value });
-            }
-            item.value = (index == event.target.name) ? event.target.value : item.value;
-            return item;
-        }));
-    };
-
-    const handleClickFavorite = (index) => {
-        setItems(items.map((item, ind) => {
-            if (index == ind) {
-                axios.patch(`${axios.defaults.baseURL}/${(item.id)}`, { favorite: !item.favorite });
-                console.log(axios.get(`${axios.defaults.baseURL}/${(item.id)}`));
-            }
-            item.favorite = (index == ind) ? !item.favorite : item.favorite;
-            return item;
-        }));
-        console.log(items);
-    };
-
-    const handleClickRemove = (index) => {
-        setItems(items.filter((item, ind) => { 
-            return !(ind == index && axios.delete(`${axios.defaults.baseURL}/${(item.id)}`));
-        }));
-    };
-
     return (
-        <div className="main">
-            <Header 
-                task={task} 
-                onChange={handleChangeTask} 
-                onClick={handleClickAdd} 
-                onKeyPress={handleKeyPressEnter} 
-                autoFocus={true} 
-            />
-            <List 
-                items={items} 
-                onChange={{Checked: handleChangeChecked, Items: handleChangeItems}} 
-                onClick={{Favorite: handleClickFavorite, Remove: handleClickRemove}}
-            />
-        </div>
+        <Router>
+            <div className="nav">
+                <ul>
+                    <li>
+                        <NavLink id="link-currient-tasks" className="link" to="/">Current tasks</NavLink>                        
+                    </li>
+                    <li>
+                        <NavLink id="link-completed-tasks" className="link" to="/completed-tasks">Completed tasks</NavLink>
+                    </li>
+                </ul>
+            </div>
+            <Switch>
+                <Route exact path="/">
+                    <Main />
+                </Route>
+                <Route path="/completed-tasks">
+                    <CompletedTasks />
+                </Route>
+            </Switch>
+        </Router>
     );
 }
---
+++ src/pages/Main.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Header from './../Header';
+import List from './../List';
+import './Main.scss';
+
+export default function Main() {
+    const [task, setTask] = useState("");
+    const [items, setItems] = useState([]);
+    
+    axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+    
+    const handleChangeTask = (event) => {
+        setTask(event.target.value);
+    };
+    useEffect(() => {
+        axios.get(`${axios.defaults.baseURL}`)
+        .then(response => {
+            setItems(response.data);
+        });
+    }, []);
+
+    const handleClickAdd = (value) => {
+        if (value === "")
+            return;
+
+        axios.post(`${axios.defaults.baseURL}`, { 
+                value: value, 
+                checked: false,
+                favorite: false 
+            })
+            .then(response => {
+                setItems(items.concat(response.data));
+            });
+
+        setTask("");
+    };
+
+    const handleKeyPressEnter = (event) => {
+        if (event.key === "Enter")
+            handleClickAdd(event.target.value);
+    };
+
+    const handleChangeChecked = (event) => {
+        setItems(items.map((item, index) => {
+            if (index == event.target.name) {
+                axios.patch(`${axios.defaults.baseURL}/${(item.id)}`, {checked: event.target.checked, favorite: false });
+            }
+            item.checked = (index == event.target.name) ? event.target.checked : item.checked;
+            return item;
+        }));
+    };
+
+    const handleChangeItems = (event) => {
+        setItems(items.map((item, index) => {
+            if (index == event.target.name) {
+                axios.patch(`${axios.defaults.baseURL}/${(item.id)}`, { value: event.target.value });
+            }
+            item.value = (index == event.target.name) ? event.target.value : item.value;
+            return item;
+        }));
+    };
+
+    const handleClickFavorite = (index) => {
+        setItems(items.map((item, ind) => {
+            if (index == ind) {
+                axios.patch(`${axios.defaults.baseURL}/${(item.id)}`, { favorite: !item.favorite });
+                console.log(axios.get(`${axios.defaults.baseURL}/${(item.id)}`));
+            }
+            item.favorite = (index == ind) ? !item.favorite : item.favorite;
+            return item;
+        }));
+        console.log(items);
+    };
+
+    const handleClickRemove = (index) => {
+        setItems(items.filter((item, ind) => { 
+            return !(ind == index && axios.delete(`${axios.defaults.baseURL}/${(item.id)}`));
+        }));
+    };
+
+    return (
+        <div className="main">
+            <Header 
+                task={task} 
+                onChange={handleChangeTask} 
+                onClick={handleClickAdd} 
+                onKeyPress={handleKeyPressEnter} 
+                autoFocus={true} 
+            />
+            <List 
+                items={items} 
+                onChange={{Checked: handleChangeChecked, Items: handleChangeItems}} 
+                onClick={{Favorite: handleClickFavorite, Remove: handleClickRemove}}
+            />
+        </div>
+    );
+}
